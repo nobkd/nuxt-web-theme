72e832372e237bc873afa851ca209d8ec458ae77
nobkd <44443899+nobkd@users.noreply.github.com>
2023-02-03 20:03:48 +0100

    chore: formatting

--- app.vue
+++ app.vue
@@ -18,168 +18,217 @@ main
 
 <style>
 /*https://github.com/unocss/unocss/blob/main/packages/reset/antfu.css*/
-*, ::before, ::after {
-  box-sizing: border-box;
-  border-width: 0;
-  border-style: solid;
-  border-color: currentColor;
+*,
+::before,
+::after {
+    box-sizing: border-box;
+    border-width: 0;
+    border-style: solid;
+    border-color: currentColor;
 }
 html {
-  line-height: 1.5;
-  -webkit-text-size-adjust: 100%;
-  -moz-tab-size: 4;
-  tab-size: 4;
-  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-  scrollbar-gutter: stable;
+    line-height: 1.5;
+    -webkit-text-size-adjust: 100%;
+    -moz-tab-size: 4;
+    tab-size: 4;
+    font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+        Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',
+        'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+    scrollbar-gutter: stable;
 }
 body {
-  margin: 0;
-  line-height: inherit;
+    margin: 0;
+    line-height: inherit;
 }
 hr {
-  height: 0;
-  color: inherit;
-  border-top-width: 1px;
+    height: 0;
+    color: inherit;
+    border-top-width: 1px;
 }
 abbr:where([title]) {
-  text-decoration: underline dotted;
+    text-decoration: underline dotted;
 }
-h1, h2, h3, h4, h5, h6 {
-  font-size: inherit;
-  font-weight: inherit;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    font-size: inherit;
+    font-weight: inherit;
 }
 a {
-  color: inherit;
-  text-decoration: inherit;
+    color: inherit;
+    text-decoration: inherit;
 }
-b, strong {
-  font-weight: bolder;
+b,
+strong {
+    font-weight: bolder;
 }
-code, kbd, samp, pre {
-  font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
-  font-size: 1em;
+code,
+kbd,
+samp,
+pre {
+    font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono',
+        'Courier New', monospace;
+    font-size: 1em;
 }
 small {
-  font-size: 80%;
+    font-size: 80%;
 }
-sub, sup {
-  font-size: 75%;
-  line-height: 0;
-  position: relative;
-  vertical-align: baseline;
+sub,
+sup {
+    font-size: 75%;
+    line-height: 0;
+    position: relative;
+    vertical-align: baseline;
 }
 sub {
-  bottom: -0.25em;
+    bottom: -0.25em;
 }
 sup {
-  top: -0.5em;
+    top: -0.5em;
 }
 table {
-  text-indent: 0;
-  border-color: inherit;
-  border-collapse: collapse;
+    text-indent: 0;
+    border-color: inherit;
+    border-collapse: collapse;
 }
-button, input, optgroup, select, textarea {
-  font-family: inherit;
-  font-size: 100%;
-  line-height: inherit;
-  color: inherit;
-  margin: 0;
-  padding: 0;
+button,
+input,
+optgroup,
+select,
+textarea {
+    font-family: inherit;
+    font-size: 100%;
+    line-height: inherit;
+    color: inherit;
+    margin: 0;
+    padding: 0;
 }
-button, select {
-  text-transform: none;
+button,
+select {
+    text-transform: none;
 }
-button, [type='button'], [type='reset'], [type='submit'] {
-  -webkit-appearance: button;
-  background-color: transparent;
-  background-image: none;
+button,
+[type='button'],
+[type='reset'],
+[type='submit'] {
+    -webkit-appearance: button;
+    background-color: transparent;
+    background-image: none;
 }
 :-moz-focusring {
-  outline: auto;
+    outline: auto;
 }
 :-moz-ui-invalid {
-  box-shadow: none;
+    box-shadow: none;
 }
 progress {
-  vertical-align: baseline;
+    vertical-align: baseline;
 }
-::-webkit-inner-spin-button, ::-webkit-outer-spin-button {
-  height: auto;
+::-webkit-inner-spin-button,
+::-webkit-outer-spin-button {
+    height: auto;
 }
 [type='search'] {
-  -webkit-appearance: textfield;
-  outline-offset: -2px;
+    -webkit-appearance: textfield;
+    outline-offset: -2px;
 }
 ::-webkit-search-decoration {
-  -webkit-appearance: none;
+    -webkit-appearance: none;
 }
 ::-webkit-file-upload-button {
-  -webkit-appearance: button;
-  font: inherit;
+    -webkit-appearance: button;
+    font: inherit;
 }
 summary {
-  display: list-item;
+    display: list-item;
 }
-blockquote, dl, dd, h1, h2, h3, h4, h5, h6, hr, figure, p, pre {
-  margin: 0;
+blockquote,
+dl,
+dd,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+hr,
+figure,
+p,
+pre {
+    margin: 0;
 }
 fieldset {
-  margin: 0;
-  padding: 0;
+    margin: 0;
+    padding: 0;
 }
 legend {
-  padding: 0;
+    padding: 0;
 }
-ol, ul, menu {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+ol,
+ul,
+menu {
+    list-style: none;
+    margin: 0;
+    padding: 0;
 }
 textarea {
-  resize: vertical;
+    resize: vertical;
 }
-input::placeholder, textarea::placeholder {
-  opacity: 1;
-  color: #9ca3af;
+input::placeholder,
+textarea::placeholder {
+    opacity: 1;
+    color: #9ca3af;
 }
-button, [role="button"] {
-  cursor: pointer;
+button,
+[role='button'] {
+    cursor: pointer;
 }
 :disabled {
-  cursor: default;
+    cursor: default;
 }
-img, svg, video, canvas, audio, iframe, embed, object {
-  display: block;
-  vertical-align: middle;
+img,
+svg,
+video,
+canvas,
+audio,
+iframe,
+embed,
+object {
+    display: block;
+    vertical-align: middle;
 }
-img, video {
-  max-width: 100%;
-  height: auto;
+img,
+video {
+    max-width: 100%;
+    height: auto;
 }
 [hidden] {
-  display: none;
+    display: none;
 }
 /* Anthony's addition */
 .dark {
-  color-scheme: dark;
+    color-scheme: dark;
 }
-.dark ::-moz-selection  {
-  background: #444;
+.dark ::-moz-selection {
+    background: #444;
 }
 .dark ::selection {
-  background: #444;
+    background: #444;
 }
-div[column=""] { /*added `[column=""]`, to prevent auto flexing*/
-  display: flex;
-  flex-direction: column;
+div[column=''] {
+    /*added `[column=""]`, to prevent auto flexing*/
+    display: flex;
+    flex-direction: column;
 }
-div[row=""] {
-  display: flex;
-  flex-direction: row;
+div[row=''] {
+    display: flex;
+    flex-direction: row;
 }
-div[block=""] {
-  display: block;
+div[block=''] {
+    display: block;
 }
 </style>
 
@@ -257,7 +306,7 @@ h1, h2, h3, h4, h5, h6
     --inset-shadow: rgba(0, 0, 0, var(--opacity-bright)) 0 2px 4px 0 inset
     --shadow: 0 0 0.1rem grey
     --text-shadow: 0 0 1px var(--text)
-    
+
     --text: black
     --bg: white
 
