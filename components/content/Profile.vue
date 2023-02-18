b5cb946444611573379dfa4f5c5771fbf8339943
nobkd <44443899+nobkd@users.noreply.github.com>
2023-02-18 18:15:35 +0100

    feat: add grid and profile components

---
+++ components/content/Profile.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+defineProps({
+    img: {
+        type: String,
+        default: 'person-placeholder.svg',
+    },
+});
+</script>
+
+<template>
+    <div class="container">
+        <img :src="img" alt="" />
+        <div class="text">
+            <p class="name"><ContentSlot :use="$slots.name" unwrap="p" /></p>
+            <p class="description">
+                <ContentSlot :use="$slots.description" unwrap="p" />
+            </p>
+        </div>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+.container
+    display: grid
+    grid-template-columns: 4rem 1fr
+    gap: 0.75rem
+
+    padding: 1rem
+    border-radius: var(--radii-xl)
+    border: 1px solid var(--elements-border-primary-static)
+    background-color: var(--elements-backdrop-background)
+
+img
+    border-radius: 100%
+    height: 4rem
+    width: 4rem
+
+.name
+    font-weight: bold
+
+.description
+    font-size: var(--text-sm-fontSize)
+    line-height: var(--text-sm-lineHeight)
+    font-weight: var(--fontWeight-medium)
+    color: var(--elements-text-secondary-color-static)
+</style>
---
+++ components/content/prose/Grid.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ContentSlot :use="$slots.default" unwrap="p" />
+    </div>
+</template>
+
+<style lang="sass" scoped>
+div
+    display: grid
+    justify-content: center
+    gap: 0.25rem
+
+    grid-template-columns: 1fr
+    margin: 0.25rem 0
+
+    @media (min-width: 475px)
+        grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr))
+</style>
