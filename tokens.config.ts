9769dfa6cb97612e9df261357854831958ed77af
nobkd <44443899+nobkd@users.noreply.github.com>
2023-03-09 21:50:57 +0100

    feat: add back palette function with theme-colors

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,19 +1,9 @@
 import { defineTheme } from 'pinceau';
+import { getColors } from 'theme-colors';
 
 export default defineTheme({
     color: {
-        primary: {
-            900: '#002c1a',
-            800: '#005331',
-            700: '#007a48',
-            600: '#00a15f',
-            500: '#00DC82',
-            400: '#17df8d',
-            300: '#44e5a3',
-            200: '#71ecba',
-            100: '#9ff2d0',
-            50: '#ccf8e6',
-        },
+        primary: getColors('#00DC82'),
     },
     prose: {
         h1: {
